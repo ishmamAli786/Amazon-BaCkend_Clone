0f118ba8cf023fd162354b5d51829bc0f043fdac
Ishmam Ali <ishmamalikhan@gmail.com>
2021-02-17 21:20:56 +0500

    Added Database File

---
+++ database.js
@@ -0,0 +1,12 @@
+///include library
+const mongoose=require('mongoose');
+const DB_URL=process.env.DB_URL;
+
+////Establish Database Connection
+mongoose.connect(DB_URL, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex:true,useFindAndModify:true})
+.then(()=>{
+    console.log("Database Connected Successfully")
+})
+.catch(()=>{
+    console.log("Database Failed To Connect")
+})
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //// library include
 require('dotenv').config();
+require('./database');
 const express=require('express');
 const app=express();
 const port=process.env.PORT;
