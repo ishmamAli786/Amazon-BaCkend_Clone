5259c2443363871ccb3648c5b15e93a6f86ef7e0
Ishmam Ali <ishmamalikhan@gmail.com>
2021-02-17 20:46:16 +0500

    Starter Code

---
+++ index.js
@@ -0,0 +1,35 @@
+//// library include
+require('dotenv').config();
+const express=require('express');
+const app=express();
+const port=process.env.PORT;
+const cors=require('cors');
+const morgan=require('morgan');
+
+
+
+
+
+///middleware
+app.use(cors());
+app.use(morgan('dev'));
+
+
+
+///routing
+app.get('/',(req,res)=>{
+    return res.status(200).json({
+        message:"Amazon Clone REST API Home Page"
+    })
+})
+
+
+
+
+
+
+
+////start server
+app.listen(port,()=>{
+    console.log(`Server Is Running On ${port}`)
+})
