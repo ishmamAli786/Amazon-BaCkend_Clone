d52b043f0aa150db5b88437d7489e445b158495f
Ishmam Ali <ishmamalikhan@gmail.com>
2021-02-25 00:38:00 +0500

    New User Insert Into Database

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -1,58 +1,105 @@
 ///include library
-const express=require('express');
-const router=express.Router();
-const bcrypt=require('bcryptjs');
-const bodyParser=require('body-parser');
-const {check,validationResult}=require('express-validator');
-const jwt=require('jsonwebtoken');
-const moment=require('moment');
-const UserModel=require('.././models/user');
-const token_key=process.env.TOKEN_KEY;
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+const UserModel = require('.././models/user');
+const token_key = process.env.TOKEN_KEY;
 
 ///middleware setup
 router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({extended:true}));
+router.use(bodyParser.urlencoded({ extended: true }));
 
 
 
 
 //default route
-router.get('/',(req,res)=>{
-   return  res.status(200).json({
-       status:"true",
-       message:"User Default Route"
+router.get('/', (req, res) => {
+    return res.status(200).json({
+        status: "true",
+        message: "User Default Route"
     })
 })
 
 
 ///user register route
-router.post('/register',[
+//// Express Validation
+router.post('/register', [
     //check empty field
     check('username').not().isEmpty().trim().escape(),
     check('password').not().isEmpty().trim().escape(),
     //check email
     check('email').isEmail().normalizeEmail()
 
-],(req,res)=>{ 
-    const errors=validationResult(req);
+], (req, res) => {
+    const errors = validationResult(req);
     ///check errors is not empty
-    if(!errors.isEmpty()){
+    if (!errors.isEmpty()) {
         return res.status(400).json({
-            status:false,
-            errors:errors.array()
+            status: false,
+            errors: errors.array()
         })
-    }
-    const salt=bcrypt.genSaltSync(10)
-    const hashedPassword=bcrypt.hashSync(req.body.password,salt);
+    } 
 
 
-    
-    return res.status(200).json({
-            status:true,
-            data:req.body,
-            hashedPassword:hashedPassword
-        })
+    ///// insert New User
+
+    UserModel.findOne({ email: req.body.email }).then((user) => {
+        ///check email exist or not
+        if (user) {
+            return res.status(409).json({
+                status: false,
+                message: "User Email Already Exist"
+            });
+        }else{
+            const salt = bcrypt.genSaltSync(10)
+            const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+            const newUser=new UserModel({
+                username:req.body.username,
+                email:req.body.email,
+                password:hashedPassword
+            });
+            // newUser.save().then((result)=>{
+            //     return res.status(200).json({
+            //         status: True,
+            //         message: "User Registered Successfully",
+            //         result:result
+            //     });
+            // }).catch((error)=>{
+            //     return res.status(502).json({
+            //         status: false,
+            //         message: "User Failed To Register",
+            //         error:error
+            //     });
+            // });
+            newUser.save((err,result)=>{
+                if(err) throw err;
+                if(result){
+                    return res.status(200).json({
+                        status: true,
+                        message:"Data Inserted Into Database Successfully",
+                        result:result
+                    }); 
+                }else{
+                    return res.status(502).json({
+                        status: false,
+                        message:"Data Failed to Insert Into Database",
+                        error:error
+                    });
+                }
+            })
+        }
+    })
+    .catch((error)=>{
+        return res.status(502).json({
+            status: false,
+            error:error
+        });
+    });
 })
 
 
-module.exports=router;
+module.exports = router;
